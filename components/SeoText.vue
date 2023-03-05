6742c04bd21539618a5a8b2ba1f3144ad62c4381
skibekleidung-net <schlafsack24.net@surrealnetworks.io>
2023-03-05 17:09:31 +0100

    seotext

--- components/SeoText.vue
+++ components/SeoText.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container" v-if="seoData && seoData.texts">
+  <div class="container">
     <div class="row g-5">
       <div class="col-lg-8 fadeInUp" style="min-height: 400px">
-        <div v-if="seoData && seoData.texts">
-          <div v-for="(text, index) in seoData.texts" :key="index">
+        <div v-if="'content' in seoData.seotext">
+          <div v-for="(text, index) in seoData.seotext.content" :key="index">
             <div class="mb-3 mt-5 h2" v-html="text.title"></div>
             <p class="mb-4" v-html="text.text"></p>
           </div>
